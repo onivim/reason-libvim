22fc835e32a0772d5e65810c719ea69944c3ff57
bryphe <bryphe@outrunlabs.com>
2019-06-05 13:21:45 -0700

    Add example / bindings

--- example/Example.re
+++ example/Example.re
@@ -4,6 +4,6 @@ let _ = {
     print_endline("Initializing successful.");
 
     print_endline("Buffer.test");
-    Vim.Buffer.test();
+    Vim.command("e package.json");
     print_endline("Buffer.test complete");
 };
--- src/bindings.c
+++ src/bindings.c
@@ -8,3 +8,19 @@ libvim_vimInit(value unit) {
     vimInit(0, args);
     return Val_unit;
 }
+
+CAMLprim value
+libvim_vimInput(value v) {
+    char_u *s;
+    s = (char_u *)String_val(v);
+    vimInput(s);
+    return Val_unit;
+}
+
+CAMLprim value
+libvim_vimCommand(value v) {
+    char_u *s;
+    s = (char_u *)String_val(v);
+    vimExecute(s);
+    return Val_unit;
+}
